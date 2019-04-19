ece97fa38eaee1d06f76bfc996389ccc8fa962ba
xtoolNH <xtoolnihilent@gmail.com>
2019-04-19 15:35:48 +0530

    addition of --user command in requirements.txt

--- setup.bat
+++ setup.bat
@@ -1,1 +1,6 @@
-pip install -r requirements.txt --user
+ECHO Starting Emotions SDK Installation
+
+call pip install -r requirements.txt --user
+call pip install -r requirements.txt
+
+ECHO End of Emotions SDK Installation
